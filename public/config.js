a3fcbb8ee97a533530945227d8106d17dd411e5d
Lim Hong-deok <nhtour1997@gmail.com>
2026-06-01 21:08:34 +0900

    fix API port 8080 issue

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 const PROTOCOL = window.location.protocol
 const HOST_NAME = window.location.hostname
 const BASE_URL = (HOST_NAME !== 'localhost' && HOST_NAME !== '192.168.0.198') ? `${PROTOCOL}//${HOST_NAME}` : 'https://nhsafe-dev.maot.kr'
-const BASE_API_URL = `${BASE_URL}:8080`
+const BASE_API_URL = `${BASE_URL}`
 
 const config = { BASE_URL, BASE_API_URL, HOST_NAME }
 export  default config
